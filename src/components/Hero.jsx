207f866369b0cd74f15d6ae04bdf5048edc2c22b
leonardobabinski <60757857+leonardobabinski@users.noreply.github.com>
2025-07-15 15:37:59 -0300

    h

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -54,7 +54,7 @@ const Hero = () => {
 
           {/* Imagem à direita */}
           <div className="hidden md:flex relative flex justify-center sm:justify-end self-end overflow-visible
-          w-full md:max-w-[80%] lg:max-w-[60%]
+          w-full md:max-w-[60%] lg:max-w-[40%]
           md:absolute md:bottom-[-40px] md:right-[-100px] lg:right-[-100px]"
           >
             <img
