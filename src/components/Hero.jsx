ea43a118bb1c9adc4d19e1b6c2cbb91961f0d0e8
leonardobabinski <60757857+leonardobabinski@users.noreply.github.com>
2025-07-15 17:49:23 -0300

    h

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -54,8 +54,8 @@ const Hero = () => {
 
           {/* Imagem à direita */}
             <div className="hidden md:flex relative justify-center sm:justify-end self-end overflow-visible
-                w-full lg:max-w-[70%]
-                md:absolute md:bottom-[-40px] md:right-[-100px] lg:right-[-150px]"
+                w-full 
+                md:absolute md:bottom-[-40px] md:right-[-150px] lg:right-[-150px]"
             >
               <img
                 src={profilePhoto}
