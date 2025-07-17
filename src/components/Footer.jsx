41874f8ac59f93e297658d9ad9468b14ebe7e824
leonardobabinski <60757857+leonardobabinski@users.noreply.github.com>
2025-07-17 11:46:38 -0300

    a

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,6 @@ const Footer = () => {
       <div className="container-custom">
         <div className="text-center">
           <div className="text-white/80 text-sm">
-            <div id="google_translate_element"style={{ position: 'absolute', top: 10, right: 10 }}>
-            </div>
             © {currentYear} Leonardo Babinski. All rights reserved.
           </div>
         </div>
